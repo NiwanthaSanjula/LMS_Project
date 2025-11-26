77121ec9ad02d2e6f6d3ee4ab97053a03fb481e6
Niwantha Sanjula <niwanthasanjulaofficial@gmail.com>
2025-11-26 13:43:18 +0530

    Create educator footer

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Edu Footer</h1>
-    </div>
+    <footer className='flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 py-4 border-t'>
+      <div className='flex items-center gap-4'>
+        <img src={assets.logo} alt="" className='hidden md:block w-20'/>
+
+        <div className='hidden md:block h-7 w-px bg-gray-500/50'></div>
+
+        <p>Copyright 2025 &copy; GreatStack.All Right Reserved.</p>
+      </div>
+
+      <div className='flex items-center gap-3 max-md:mt-4'>
+        <a href="#">
+          <img src={assets.facebook_icon} alt="facebook"/>
+        </a>
+
+        <a href="#">
+          <img src={assets.twitter_icon} alt="facebook"/>
+        </a>
+
+        <a href="#">
+          <img src={assets.instagram_icon} alt="facebook"/>
+        </a>
+      </div>
+    </footer>
   )
 }
 
