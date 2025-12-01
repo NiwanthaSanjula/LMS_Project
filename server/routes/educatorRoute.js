37944ed6250d755c7ef217a50bbcf523ea2da6b9
Niwantha Sanjula <niwanthasanjulaofficial@gmail.com>
2025-12-01 20:45:00 +0530

    created course model and add course API

--- server/routes/educatorRoute.js
+++ server/routes/educatorRoute.js
@@ -1,9 +1,14 @@
 import express from 'express'
-import { updateRoleToEducator } from '../controllers/educatorContoller.js'
+import { addCourse, updateRoleToEducator } from '../controllers/educatorContoller.js'
+import upload from '../configs/multer.js'
+import { protectEducator } from '../middlewares/authMiddleware.js'
 
 const educatorRouter = express.Router()
 
 //Add educator role
 educatorRouter.get('/update-role', updateRoleToEducator)
+educatorRouter.post('/add-course', upload.single('image'), protectEducator, addCourse)
+
+
 
 export default educatorRouter
