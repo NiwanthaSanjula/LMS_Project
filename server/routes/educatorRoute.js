8eb462abe7965f15a4e90eab1d452a30d55fde9f
Niwantha Sanjula <niwanthasanjulaofficial@gmail.com>
2025-12-02 14:20:16 +0530

    created educator dashboard APIs

--- server/routes/educatorRoute.js
+++ server/routes/educatorRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addCourse, getEducatorCourses, updateRoleToEducator } from '../controllers/educatorContoller.js'
+import { addCourse, educatorDashboard, getEducatorCourses, getEnrolledStudentsData, updateRoleToEducator } from '../controllers/educatorContoller.js'
 import upload from '../configs/multer.js'
 import { protectEducator } from '../middlewares/authMiddleware.js'
 
@@ -9,6 +9,10 @@ const educatorRouter = express.Router()
 educatorRouter.get('/update-role', updateRoleToEducator)
 educatorRouter.post('/add-course', upload.single('image'), protectEducator, addCourse)
 educatorRouter.get('/courses', protectEducator, getEducatorCourses)
+educatorRouter.get('/dashboard', protectEducator, educatorDashboard)
+educatorRouter.get('/enrolled-students', protectEducator, getEnrolledStudentsData)
+
+
 
 
 
