40931f6e4c72322a32abd1d671c8594581d54d5f
Niwantha Sanjula <niwanthasanjulaofficial@gmail.com>
2025-12-03 22:29:10 +0530

    fix stripe webhook bugs 12

--- server/server.js
+++ server/server.js
@@ -9,27 +9,32 @@ import connectCloudinary from './configs/cloudinary.js'
 import courseRouter from './routes/courseRoute.js'
 import userRouter from './routes/userRoutes.js'
 
+//init express
 const app = express()
 
+//connect to database
 await connectDB()
 await connectCloudinary()
+
+//middleware
 app.use(cors())
+app.use(clerkMiddleware())
 
-// --- WEBHOOKS MUST COME BEFORE express.json() ---
-app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
-app.post('/clerk', express.json(), clerkWebHooks)
-
-// JSON parsing AFTER webhooks
-app.use(express.json())
-
-// Clerk only for protected routes
-app.use('/api', clerkMiddleware())
-
-// Routes
-app.use('/api/educator', educatorRouter)
-app.use('/api/course', courseRouter)
-app.use('/api/user', userRouter)
-
+//Routes
 app.get('/', (req, res) => res.send("API Working"))
+app.post('/clerk', express.json(), clerkWebHooks)
+app.use('/api/educator', express.json(), educatorRouter)
+app.use('/api/course', express.json(), courseRouter)
+app.use('/api/user', express.json(), userRouter)
+app.post('/stripe', express.raw({type:'application/json'}), stripeWebhooks )
 
-app.listen(5000, () => console.log("Server running"))
+
+
+
+//PORT
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+})
+
